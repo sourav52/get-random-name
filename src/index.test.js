56338b532d1e561755dccfd16f1d9ba051d05bb3
Sourav Kumar Dash <dash.sourav126@gmail.com>
2017-12-03 23:48:12 +0530

    chore(releasing): add travis.yml,convetional changelog
    closes #1

--- src/index.test.js
+++ src/index.test.js
@@ -5,4 +5,27 @@ describe("pokemon-names",function() {
 	it("should work",function() {
 		expect(true).to.be.true;
 	});
+
+	describe("all",function() {
+		it('should be an array of strings',function() {
+			expect(pokemon.all).to.satisfy(isArrayOfStrings);
+
+			function isArrayOfStrings(array) {
+				return array.every(function(item) {
+					return typeof item === 'string';
+				});
+			}
+		});
+
+		it('should contain `Boomerang`',function() {
+			expect(pokemon.all).to.include('Boomerang');
+		});
+	});
+
+	describe('random',function() {
+		it('should return a random name from the pokemon.all',function() {
+			var randomItem = pokemon.random();
+			expect(pokemon.all).to.include(randomItem);
+		});
+	});
 });
