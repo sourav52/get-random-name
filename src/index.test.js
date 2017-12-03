31f25b313b011448b4305fd34f74e066117996e9
Sourav Kumar Dash <dash.sourav126@gmail.com>
2017-12-03 22:00:08 +0530

    added test script

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var pokemon = require('./index');
+
+describe("pokemon-names",function() {
+	it("should work",function() {
+		expect(true).to.be.true;
+	});
+});
